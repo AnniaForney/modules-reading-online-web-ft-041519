4dd6a4ca64510e2908404aded59b230367ff3f31
Annia Forney <anniaforney@gmail.com>
2019-05-11 22:57:38 +0000

    Automatically backed up by Learn

--- lib/class_methods_module.rb
+++ lib/class_methods_module.rb
@@ -1,6 +1,13 @@
 module MetaDancing
 
-  def metadata
-    "This class produces objects that love to dance."
+  class Dancer
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
   end
+
+  class Kid
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
 end
+end 
+  ####
